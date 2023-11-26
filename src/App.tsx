1cf7acc5dd0bd8b02fb60518284d3a39ebcddfee
Armin <Armin.Fullstack@gmail.com>
2023-11-26 15:48:41 +0330

    feat: implemented reusable Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./styles/Heading";
+import Button from "./styles/Button";
+
 export default function App() {
-  return null;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check in and out</Heading>
+        <Heading as="h3">Form</Heading>
+        <Button size="small" variation="danger">
+          Click in
+        </Button>
+        <Button>Click out</Button>
+      </div>
+    </>
+  );
 }
