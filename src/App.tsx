4c0cf6a41cc1f2a1774c8bf3c35fbe8238efff72
Armin <Armin.Fullstack@gmail.com>
2023-11-26 20:32:01 +0330

    feat: implemented AppLayout

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,24 @@ import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 export default function App() {
   return (
     <>
       <GlobalStyles />
       <Routes>
-        <Route index element={<Navigate replace to="dashboard" />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="account" element={<Account />} />
-        <Route path="bookings" element={<Bookings />} />
-        <Route path="cabins" element={<Cabins />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="user" element={<Users />} />
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="account" element={<Account />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="user" element={<Users />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+
         <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
